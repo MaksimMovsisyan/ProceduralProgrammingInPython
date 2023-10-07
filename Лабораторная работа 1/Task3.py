645984d04498b67e53029fa838ce615de5b218f0
MaksimMovsisyan <147267581+MaksimMovsisyan@users.noreply.github.com>
2023-10-07 19:41:13 +0300

    Add files via upload

---
+++ Лабораторная работа 1/Task3.py
@@ -0,0 +1,6 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+# TODO Разделите участников на две команды
+NumberOfPlayers = len(list_players)
+print(list_players[0:3])
+print(list_players[3:6])
